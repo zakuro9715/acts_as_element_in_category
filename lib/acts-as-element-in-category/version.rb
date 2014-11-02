2c9d1b4ff95f3497df97ae088accc412dcbecf5f
YuZakuro <zakuro@yuzakuro.me>
2014-11-02 23:05:26 +0900

    first commit

---
+++ lib/acts-as-element-in-category/version.rb
@@ -0,0 +1,3 @@
+module ActsAsElementInCategory
+  VERSION = "0.0.1"
+end
---
+++ lib/category.rb
@@ -0,0 +1,13 @@
+require "acts/as/element/in/category/version"
+
+module Acts
+  module As
+    module Element
+      module In
+        module Category
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
