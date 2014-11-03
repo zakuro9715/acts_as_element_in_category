3cc4ee26e6fcbe8ecd99f173a6e9200f4d9d62a9
YuZakuro <zakuro@yuzakuro.me>
2014-11-03 18:50:13 +0900

    Create spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'active_record'
+require 'acts-as-element-in-category'
+
+
+RSpec.configure do |config|
+  config.color = true
+  config.mock_framework = :rspec
+  config.before(:all) {
+    ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+  }
+end
